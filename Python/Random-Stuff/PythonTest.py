55d2270fb4e0e013f6f5ebdb2217cacbaa4501ec
Ahendall <adham1.eldalil@gmail.com>
2021-01-27 20:56:22 +0400

    Create PythonTest.py

---
+++ Python/Random-Stuff/PythonTest.py
@@ -0,0 +1,8 @@
+from random import randint
+
+X = randint(1, 20)
+if X > 10:
+    print ("The value of X is greater than 10 (Value is ", X, ")!")
+
+elif X < 10:
+    print ("The value of X is less than 10!(Value is ", X, ")!")
