ae2926c3ada5b985ad4787761a20a6b0ee1255a0
Ahendall <62162610+Ahendall@users.noreply.github.com>
2021-04-21 09:17:21 +0400

    Create MultiLine String Test.py

---
+++ Unit 2 - Python/Lesson 3/MultiLine String Test.py
@@ -0,0 +1,13 @@
+choice = 0
+print ('''
+You are General Anakin Skywalker During the battle above Coruscant.
+
+You are on the way to Seperatist General Grievous' ship.
+Your master, General Kenobi, has had his Starfighter hit by buzz droids
+
+Will you save him?
+(Type: yes | no)
+''')
+
+choice = input()
+print (choice)
